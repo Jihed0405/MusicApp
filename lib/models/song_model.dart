8e3027219d978bd75dc82f850c2e94cc54a6ca18
jihed ben othmen <jihed0405@gmail.com>
2023-04-28 02:10:38 +0100

    init SongCard

---
+++ lib/models/song_model.dart
@@ -0,0 +1,21 @@
+class Song {
+  final String title;
+  final String description;
+  final String url;
+  final String coverUrl;
+
+  Song({
+    required this.title,
+     required this.description, 
+     required this.url,
+     required this.coverUrl});
+
+     static List<Song> songs =[
+      Song(title: 'Easy on me', description: 'Easy on me',
+       url:'assets/music/Adele - Easy On Me.mp3', coverUrl: 'assets/images/Adele - Easy On Me.jpg'),
+       Song(title: 'Positions',
+        description: 'Positions', 
+        url: 'assets/music/Ariana Grande - positions.mp3',
+         coverUrl: 'assets/images/Ariana Grande - positions.jpg')
+     ];
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:music_app/models/song_model.dart';
 import 'package:music_app/router.dart';
+
+import '../widgets/section_headers.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -11,21 +14,102 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    List<Song> songs = Song.songs;
     return Container(
-      decoration:BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
             Colors.lightBlue.shade800.withOpacity(0.8),
             Colors.lightBlue.shade200.withOpacity(0.8),
-        ])
-      ),
+          ])),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        appBar:const _CustomAppbar(),
-        bottomNavigationBar:  const _CustomNavBar(),
-        body:Container(),
+        appBar: const _CustomAppbar(),
+        bottomNavigationBar: const _CustomNavBar(),
+        body: SingleChildScrollView(
+            child: Column(
+          children: [
+            _DiscoverMusic(),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0,top:20.0,bottom: 20.0),
+              child: Column(
+                children:  [
+                  const Padding(
+                    padding: EdgeInsets.only(right: 20.0),
+                    child: SectionHeader(title:"Trending Music",),
+                  ),
+                   const SizedBox(height: 20),
+                  SizedBox(
+                  height:MediaQuery.of(context).size.height * 0.27,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: songs.length,
+                    itemBuilder: (context,index){
+                      return Container(
+                        width: MediaQuery.of(context).size.width * 0.45,
+                        decoration: BoxDecoration(
+                          image:DecorationImage(
+                            image: AssetImage(
+                              songs[index].coverUrl,
+                            ),
+                            fit: BoxFit.cover,
+                            )
+                        ),
+                      );
+                    }
+                    ),
+                     )
+                ],
+              ),
+            )
+          ],
+        )),
+      ),
+    );
+  }
+}
+
+class _DiscoverMusic extends StatelessWidget {
+  const _DiscoverMusic({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Welcome', style: Theme.of(context).textTheme.bodyLarge),
+          const SizedBox(height: 5),
+          Text(
+            'Enjoy your favorite music',
+            style: Theme.of(context)
+                .textTheme
+                .titleLarge!
+                .copyWith(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 20),
+          TextFormField(
+            decoration: InputDecoration(
+              isDense: true,
+              filled: true,
+              fillColor: Colors.white,
+              hintText: 'Search',
+              hintStyle: Theme.of(context)
+                  .textTheme
+                  .bodyMedium!
+                  .copyWith(color: Colors.grey.shade400),
+              prefixIcon: Icon(Icons.search, color: Colors.grey.shade400),
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                  borderSide: BorderSide.none),
+            ),
+          )
+        ],
       ),
     );
   }
@@ -39,31 +123,30 @@ class _CustomNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      type: BottomNavigationBarType.fixed,
-      backgroundColor: Colors.lightBlue.shade200,
-      unselectedItemColor: Colors.white,
-      selectedItemColor: Colors.white,
-      showSelectedLabels: false,
-      showUnselectedLabels:false,
-      items: const [
-      BottomNavigationBarItem(
-        icon: Icon(Icons.home),
-        label: 'Home',
-      ),
-       BottomNavigationBarItem(
-        icon: Icon(Icons.favorite_outline),
-        label: 'favorites',
-      ),
-       BottomNavigationBarItem(
-        icon: Icon(Icons.play_circle_outline),
-        label: 'Play',
-      ),
-       BottomNavigationBarItem(
-        icon: Icon(Icons.people_outline),
-        label: 'Profile',
-      )
-      
-    ]);
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.lightBlue.shade200,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Colors.white,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_outline),
+            label: 'favorites',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.play_circle_outline),
+            label: 'Play',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people_outline),
+            label: 'Profile',
+          )
+        ]);
   }
 }
 
@@ -75,24 +158,24 @@ class _CustomAppbar extends StatelessWidget with PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top:16.0),
+      padding: const EdgeInsets.only(top: 16.0),
       child: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        leading: const Icon (Icons.grid_view_rounded),
+        leading: const Icon(Icons.grid_view_rounded),
         actions: [
           Container(
             margin: const EdgeInsets.only(right: 20),
             child: const CircleAvatar(
               backgroundImage: NetworkImage(
-                'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
-              ),
-            ),)
+                  'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'),
+            ),
+          )
         ],
       ),
     );
   }
-  
+
   @override
   Size get preferredSize => const Size.fromHeight(56.0);
 }
