881d1d2b76de6bfbed7fa7ee9218841cb1e182f3
jihed ben othmen <jihed0405@gmail.com>
2023-04-30 12:57:38 +0100

    add playlist Screen

--- lib/models/song_model.dart
+++ lib/models/song_model.dart
@@ -1,25 +1,30 @@
 class Song {
   final String title;
   final String description;
+  final String singer;
   final String url;
   final String coverUrl;
 
   Song({
     required this.title,
-     required this.description, 
+     required this.description,
+     required this.singer, 
      required this.url,
      required this.coverUrl});
 
      static List<Song> songs =[
       Song(title: 'Easy on me', description: 'Easy on me',
+      singer: 'Adele',
        url:'assets/music/Adele - Easy On Me.mp3',
         coverUrl: 'assets/images/Adele - Easy On Me.jpg'),
        Song(title: 'Positions',
         description: 'Positions', 
+        singer: 'Ariana Grande',
         url: 'assets/music/Ariana Grande - positions.mp3',
          coverUrl: 'assets/images/Ariana Grande - positions.jpg'),
           Song(title: 'Satellite',
-        description: 'Satellite', 
+        description: 'Satellite',
+        singer: 'Bebe Rexha & Snoop Dogg', 
         url: 'assets/music/Bebe Rexha & Snoop Dogg - Satellite.mp3',
          coverUrl: 'assets/images/Bebe Rexha & Snoop Dogg - Satellite.jpg'),
          
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -37,25 +37,39 @@ class _HomeState extends State<Home> {
           children: [
             _DiscoverMusic(),
             _TrendingMusic(songs: songs),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                children: [
-                  const SectionHeader(title: "Playlists"),
-                  ListView.builder(
-                    shrinkWrap: true,
-                    padding: const EdgeInsets.only(top:20),
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemCount: playlists.length,
-                    itemBuilder: ((context,index){
-                      return PlaylistCard(playlist: playlists[index]);
-                    }),
-                  ),
-                ],  
-              ),
-            )
+            _PlaylistMusic(playlists: playlists)
           ],
         )),
+      ),
+    );
+  }
+}
+
+class _PlaylistMusic extends StatelessWidget {
+  const _PlaylistMusic({
+    super.key,
+    required this.playlists,
+  });
+
+  final List<Playlist> playlists;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          const SectionHeader(title: "Playlists"),
+          ListView.builder(
+            shrinkWrap: true,
+            padding: const EdgeInsets.only(top:20),
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: playlists.length,
+            itemBuilder: ((context,index){
+              return PlaylistCard(playlist: playlists[index]);
+            }),
+          ),
+        ],  
       ),
     );
   }
